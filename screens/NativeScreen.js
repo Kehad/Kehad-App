f2e68c074c81df2d3134ef161ddc267ad3cb85aa
Kehad <keahnney01@gmail.com>
2024-08-28 21:01:50 +0100

    34 commit app

--- screens/NativeScreen.js
+++ screens/NativeScreen.js
@@ -64,7 +64,7 @@
 //     setIsDownloading(true);
 //     const { uri } = await download.downloadAsync();
 //     console.log(uri)
-//     // AsyncStorage.removeItem("download");
+//     AsyncStorage.removeItem("download");
 //     setIsDownloaded(true);
 //   };
 
@@ -158,138 +158,138 @@
 ///////////////////// METHOD 2 --------- ////////////////////////////////////////////
 ///////////////////////// --------------------------- ////////////////////////////////////////
 
-// import { StatusBar } from "expo-status-bar";
-// import { StyleSheet, Button, View, Platform } from "react-native";
-// import * as FileSystem from "expo-file-system";
-// import { shareAsync } from "expo-sharing";
-
-// export default function App() {
-//   const downloadFromUrl = async () => {
-//     const filename = "kehad.jpeg";
-//     const result = await FileSystem.downloadAsync(
-//       "https://drive.google.com/uc?export=download&id=1_JHSQ7nsJIki8y2eiOBTw3bZayFpJa0q",
-//       FileSystem.documentDirectory + filename
-//     );
-//     console.log(result);
-
-//     save(result.uri, filename, result.headers["Content-Type"]);
-//   };
-
-//   const downloadFromAPI = async () => {
-//     const filename = "MissCoding.pdf";
-//     const localhost = Platform.OS === "android" ? "10.0.2.2" : "127.0.0.1";
-//     const result = await FileSystem.downloadAsync(
-//       `http://${localhost}:5000/generate-pdf?name=MissCoding&email=hello@tripwiretech.com`,
-//       FileSystem.documentDirectory + filename,
-//       {
-//         headers: {
-//           MyHeader: "MyValue",
-//         },
-//       }
-//     );
-//     console.log(result);
-//     save(result.uri, filename, result.headers["Content-Type"]);
-//   };
-
-//   const save = async (uri, filename, mimetype) => {
-//     if (Platform.OS === "android") {
-//       const permissions =
-//         await FileSystem.StorageAccessFramework.requestDirectoryPermissionsAsync();
-//       if (permissions.granted) {
-//         const base64 = await FileSystem.readAsStringAsync(uri, {
-//           encoding: FileSystem.EncodingType.Base64,
-//         });
-//         await FileSystem.StorageAccessFramework.createFileAsync(
-//           permissions.directoryUri,
-//           filename,
-//           mimetype
-//         )
-//           .then(async (uri) => {c
-//             await FileSystem.writeAsStringAsync(uri, base64, {
-//               encoding: FileSystem.EncodingType.Base64,
-//             });
-//           })
-//           .catch((e) => console.log(e));
-//       } else {
-//         shareAsync(uri);
-//       }
-//     } else {
-//       shareAsync(uri);
-//     }
-    
-//       shareAsync(uri);
-
-//   };
-//   return (
-//     <View style={styles.container}>
-//       <Button title="Download From URL" onPress={downloadFromUrl} />
-//       <Button title="Download From API" onPress={downloadFromAPI} />
-//       <StatusBar style="auto" />
-//     </View>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: "#fff",
-//     alignItems: "center",
-//     justifyContent: "center",
-//   },
-// });
-
-
-
-///////////////////////// --------------------------- ////////////////////////////////////////
-///////////////////// METHOD 3 --------- ////////////////////////////////////////////
-///////////////////////// --------------------------- ////////////////////////////////////////
-
-import React, { useState } from "react";
-import { Button, Text, View } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Button, View, Platform } from "react-native";
 import * as FileSystem from "expo-file-system";
+import { shareAsync } from "expo-sharing";
 
 export default function App() {
-  const [downloadProgress, setDownloadProgress] = useState(0);
-  const [fileUri, setFileUri] = useState(null);
-
-  const downloadFile = async () => {
-    const uri =
-      "https://drive.google.com/uc?export=download&id=1_JHSQ7nsJIki8y2eiOBTw3bZayFpJa0q"; // Replace with the URL of the file you want to download
-    const fileUri = FileSystem.documentDirectory + "downloadedImage.jpg"; // Path to save the file
-
-    const downloadResumable = FileSystem.createDownloadResumable(
-      uri,
-      fileUri,
-      {},
-      (progress) => {
-        const progressPercentage =
-          (progress.totalBytesWritten / progress.totalBytesExpectedToWrite) *
-          100;
-        setDownloadProgress(progressPercentage.toFixed(2));
-      }
+  const downloadFromUrl = async () => {
+    const filename = "kehad.jpeg";
+    const result = await FileSystem.downloadAsync(
+      "https://drive.google.com/uc?export=download&id=1_JHSQ7nsJIki8y2eiOBTw3bZayFpJa0q",
+      FileSystem.documentDirectory + filename
     );
+    console.log(result);
 
-    try {
-      const { uri } = await downloadResumable.downloadAsync();
-      setFileUri(uri);
-      console.log("File saved to:", uri);
-    } catch (e) {
-      console.error("Download failed:", e);
-    }
+    save(result.uri, filename, result.headers["Content-Type"]);
   };
 
+  const downloadFromAPI = async () => {
+    const filename = "MissCoding.pdf";
+    const localhost = Platform.OS === "android" ? "10.0.2.2" : "127.0.0.1";
+    const result = await FileSystem.downloadAsync(
+      `http://${localhost}:5000/generate-pdf?name=MissCoding&email=hello@tripwiretech.com`,
+      FileSystem.documentDirectory + filename,
+      {
+        headers: {
+          MyHeader: "MyValue",
+        },
+      }
+    );
+    console.log(result);
+    save(result.uri, filename, result.headers["Content-Type"]);
+  };
+
+  const save = async (uri, filename, mimetype) => {
+    if (Platform.OS === "android") {
+      const permissions =
+        await FileSystem.StorageAccessFramework.requestDirectoryPermissionsAsync();
+      if (permissions.granted) {
+        const base64 = await FileSystem.readAsStringAsync(uri, {
+          encoding: FileSystem.EncodingType.Base64,
+        });
+        await FileSystem.StorageAccessFramework.createFileAsync(
+          permissions.directoryUri,
+          filename,
+          mimetype
+        )
+          .then(async (uri) => {c
+            await FileSystem.writeAsStringAsync(uri, base64, {
+              encoding: FileSystem.EncodingType.Base64,
+            });
+          })
+          .catch((e) => console.log(e));
+      } else {
+        shareAsync(uri);
+      }
+    } else {
+      shareAsync(uri);
+    }
+    
+
+
+  };
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Download Progress: {downloadProgress}%</Text>
-      <Button title="Download File" onPress={downloadFile} />
-      {fileUri && <Text>File saved at: {fileUri}</Text>}
+    <View style={styles.container}>
+      <Button title="Download From URL" onPress={downloadFromUrl} />
+      <Button title="Download From API" onPress={downloadFromAPI} />
+      <StatusBar style="auto" />
     </View>
   );
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
+
+
 
 ///////////////////////// --------------------------- ////////////////////////////////////////
-///////////////////// METHOD 4 --------- ////////////////////////////////////////////
+///////////////////// METHOD 3  for just downloading --------- ////////////////////////////////////////////
+///////////////////////// --------------------------- ////////////////////////////////////////
+
+// import React, { useState } from "react";
+// import { Button, Text, View } from "react-native";
+// import * as FileSystem from "expo-file-system";
+
+// export default function App() {
+//   const [downloadProgress, setDownloadProgress] = useState(0);
+//   const [fileUri, setFileUri] = useState(null);
+
+//   const downloadFile = async () => {
+//     const uri =
+//       "https://drive.google.com/uc?export=download&id=1_JHSQ7nsJIki8y2eiOBTw3bZayFpJa0q"; // Replace with the URL of the file you want to download
+//     const fileUri = FileSystem.documentDirectory + "downloadedImage.jpg"; // Path to save the file
+
+//     const downloadResumable = FileSystem.createDownloadResumable(
+//       uri,
+//       fileUri,
+//       {},
+//       (progress) => {
+//         const progressPercentage =
+//           (progress.totalBytesWritten / progress.totalBytesExpectedToWrite) *
+//           100;
+//         setDownloadProgress(progressPercentage.toFixed(2));
+//       }
+//     );
+
+//     try {
+//       const { uri } = await downloadResumable.downloadAsync();
+//       setFileUri(uri);
+//       console.log("File saved to:", uri);
+//     } catch (e) {
+//       console.error("Download failed:", e);
+//     }
+//   };
+
+//   return (
+//     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+//       <Text>Download Progress: {downloadProgress}%</Text>
+//       <Button title="Download File" onPress={downloadFile} />
+//       {fileUri && <Text>File saved at: {fileUri}</Text>}
+//     </View>
+//   );
+// }
+
+
+///////////////////////// --------------------------- ////////////////////////////////////////
+///////////////////// METHOD 4 for downloading and saving --------- ////////////////////////////////////////////
 ///////////////////////// --------------------------- ////////////////////////////////////////
 // import React, { useState } from "react";
 // import { Image, Button, View, Text } from "react-native";

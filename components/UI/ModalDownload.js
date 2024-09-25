d0db0a18662bb15bb1d2d7bc4ff8498f31f4f767
Kehad <keahnney01@gmail.com>
2024-09-25 23:53:38 +0100

    commit app 54

--- components/UI/ModalDownload.js
+++ components/UI/ModalDownload.js
@@ -130,7 +130,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "rgba(0, 0, 0, 0.5)", // This gives the semi-transparent backdrop
+    backgroundColor: "rgba(0, 0, 0, 0.5)", 
   },
   button: {
     padding: 10,
